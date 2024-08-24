9c851038ee3742125d58688914541a3e15d5a744
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-08-24 10:45:49 -0300

    Ejercicio 1 corregido en clase
    falta probrar

---
+++ Practica 1/Ejercicio 1/ejericio1.py
@@ -0,0 +1,142 @@
+
+# LENGTHAPELLIDO = 16
+# LENGTHNOMBRE = 16
+# LENGTHCODIGO = 4
+# TOTALLENGTH = 36
+# CARACTERRELLENO= " "
+
+# def alta(apellido,nombre,codigo):
+#     with open('nombrearchivo.txt','a') as archivo:
+#         archivo.write(apellido.ljust(LENGTHAPELLIDO, CARACTERRELLENO))
+#         archivo.write(nombre.ljust(LENGTHNOMBRE,CARACTERRELLENO))
+#         archivo.write(codigo.ljust(LENGTHCODIGO,CARACTERRELLENO))
+
+# def leer(indice):
+#     offset= (indice-1)*TOTALLENGTH
+#     with open('nombrearchivo.txt','r') as archivo:
+#         archivo.seek(offset)
+#         apellido=archivo.read(LENGTHAPELLIDO)
+#         nombre=archivo.read(LENGTHNOMBRE)
+#         codigo=archivo.read(LENGTHCODIGO)
+#     return apellido+nombre+codigo
+    
+
+# def baja(indice_eliminar):
+#     archivo=open('practica1/nombrearchivo.txt','r')
+#     lineas= archivo.read()
+#     lineas_modificados=lineas[0:(indice_eliminar-1) * TOTALLENGTH ]+lineas[indice_eliminar*TOTALLENGTH:len(lineas)]    
+#     archivo.close()
+
+#     archivo=open('nombrearchivo.txt','w')
+#     archivo.write(lineas_modificados)
+#     archivo.close()
+
+    
+
+
+# def modificar(indice,apellido,nombre,codigo):
+#     archivo=open('nombrearchivo.txt','r')
+#     lineas= archivo.read()
+#     lineas_hasta_modificar=lineas[0:(indice-1) * TOTALLENGTH ]
+#     lineas_despues_modificar=lineas[(indice*TOTALLENGTH):len(lineas)]    
+#     archivo.close()
+
+#     archivo=open('nombrearchivo.txt','w')
+#     archivo.write(lineas_hasta_modificar)
+#     archivo.write(apellido.ljust(LENGTHAPELLIDO))
+#     archivo.write(nombre.ljust(LENGTHNOMBRE))
+#     archivo.write(codigo.ljust(LENGTHCODIGO))
+#     archivo.write(lineas_despues_modificar)
+#     archivo.close()
+
+
+#---------- Corregido en clase, FALTA ARREGLAR LAS LLAMADAS DESDE EL MAIN 
+    
+from os import write
+import os
+TAMANIOAPELLIDO = 16
+TAMANIONOMBRE = 16
+TAMANIOCODIGO = 4
+TAMANIODATO= TAMANIOCODIGO + TAMANIOAPELLIDO + TAMANIONOMBRE
+PATH = "Archivo.txt"
+"""si multiplico 36 por la cantidad de datos,obtengo el tamaño del archivo"""
+
+
+def readByPK(index):
+    pos = offset(index)
+    return readByOffset(pos)
+
+def readByOffset(pos):
+    with open(PATH,"rt",encoding="utf-8") as archivo:
+        archivo.seek(pos)
+        apellido = archivo.read(TAMANIOAPELLIDO).strip()
+        nombre = archivo.read(TAMANIONOMBRE).strip()
+        codigo = archivo.read(TAMANIOCODIGO).strip()
+        return apellido,nombre,codigo
+
+def writeByPK(index,apellido,nombre,codigo):
+    if index is not None:
+        pos = offset(index)
+    else:
+        pos = None
+    writeByOffset(pos,apellido,nombre,codigo)
+
+def writeByOffset(pos,apellido,nombre,codigo):
+    with open(PATH,"r+b") as archivo:
+        if pos is None:
+            archivo.seek(0,2)
+        else:
+            archivo.seek(pos)
+        archivo.write(apellido.ljust(TAMANIOAPELLIDO).encode("utf-8"))
+        archivo.write(nombre.ljust(TAMANIONOMBRE).encode("utf-8"))
+        archivo.write(codigo.ljust(TAMANIOCODIGO).encode("utf-8"))
+
+def offset(index):
+    offset1 = TAMANIODATO * (index-1)
+    return offset1
+
+
+def insert(new_apellido,new_nombre,new_codigo):
+    writeByPK(None,new_apellido, new_nombre, new_codigo)
+
+def delete(index):
+    #corregido
+    pos = offset(index)
+    with open(PATH,"r+b") as archivo:
+        archivo.seek(-TAMANIODATO,2)
+        resto = archivo.read()
+        archivo.seek(pos)
+        archivo.write(resto)
+        archivo.seek(-TAMANIODATO,2)
+        archivo.truncate()
+
+
+def update(index, new_apellido, new_nombre, new_codigo):
+    pos = offset(index)
+    old_apellido,old_nombre,old_codigo = readByPK(index)
+    if new_apellido is None:
+        new_apellido = old_apellido
+
+    if new_nombre is None:
+        new_nombre = old_nombre
+
+    if new_codigo is None:
+        new_codigo = old_codigo
+
+    writeByPK(index,new_apellido,new_nombre,new_codigo)
+
+
+
+def mostrarArchivo():
+    index = 1
+    while True:
+        apellido,nombre,codigo = readByPK(index)
+        if not codigo:
+            break
+        print("----------------------")
+        print("Cliente ", index)
+        print("Apellido: ", apellido)
+        print("Nombre: ", nombre)
+        print("Código: ", codigo)
+        print("----------------------")
+        index += 1
--- Practica 1/Ejercicio 1/main_ej1.py
+++ Practica 1/Ejercicio 1/main_ej1.py
@@ -2,24 +2,32 @@ import ejericio1
 
 
        
-print("El programa ha comenzado.") 
-accion= input("Bienvenido seleccione una opcion: A(alta) | B(baja) | M(modificar) | S(salir): ")
+def mostrarMenu():
+    print("ABM de Archivo de Metadata\n")
+    print("0- Salir\n" +
+          "1 - Mostrar archivo\n" +
+          "2 - Alta\n" + 
+          "3 - Modificacion\n" +
+          "4 - Baja\n")
 
-while(accion.upper != 'S'):
-    if accion.upper() == 'A':
+
+while(accion.upper != '0'):
+    if accion.upper() == '2':
         nombre=input("Ingrese el nombre: ")
         apellido=input("Ingrese el apellido: ")
         codigo =input("Ingrese codigo: ")
-        ejericio1.alta(apellido,nombre,codigo)
-    elif accion.upper() == 'B':
+        ejericio1.insert(apellido,nombre,codigo)
+    elif accion.upper() == '4':
         indice=int(input('Ingrese el indice que desea eliminar: '))
-        ejericio1.baja(indice)
-    elif accion.upper() == 'M':
+        ejericio1.delete(indice)
+    elif accion.upper() == '3':
         indice=int(input('Ingrese el indice que desea eliminar: '))
         nombre=input("Ingrese el nombre: ")
         apellido=input("Ingrese el apellido: ")
         codigo =input("Ingrese codigo: ")
-        ejericio1.modificar(indice,apellido,nombre,codigo)
+        ejericio1.update(indice,apellido,nombre,codigo)
+    elif accion.upper == '1':
+        ejericio1.mostrarArchivo()
     else:
         print("Opción no válida.")
         break;
