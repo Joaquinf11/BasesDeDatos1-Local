cb9dd093fc5f3a1e1d408b31afc26ae49dcabadc
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-08-29 10:11:38 -0300

    EJ 3- write by pg y offset (SIN PROBRAR)

--- Practica 1/Ejercicio 3/ejercicio3.py
+++ Practica 1/Ejercicio 3/ejercicio3.py
@@ -6,17 +6,18 @@ LENGTH_CARACTERES = 4
 LENGTH_COLUMNA = 20 # tamaño de columna, CHEUQEAR creo que se calculaba con alguna de las constantes anteriores.
                     # creo que seria length nombre columna + length caracteres
 
+PATH_ARCHIVO= 'nombrearchivo.txt'
 
 def generarArchivo():
     cantColumnas=input("Ingrese la cantidad de columnas: ")
-    with open('nombrearchivo.txt','w') as archivo:
+    with open(PATH_ARCHIVO,'w') as archivo:
         archivo.write(cantColumnas.ljust(LENGTH_CANT_COLUMNAS))
         for i in range(int(cantColumnas)):
             archivo.write(input(f"inserte el titulo de la columna numero {i}: ").ljust(LENGTH_NOMBRE_COLUMNA))
             archivo.write(input(f"inserte la cantidad de caracteres utilizados por dato en la columna numero {i}: ").ljust(LENGTH_CARACTERES))
 
 def lengthHeader():
-    with open('nombrearchivo.txt','r') as archivo:
+    with open(PATH_ARCHIVO,'r') as archivo:
         cantColumnas=archivo.read(LENGTH_CANT_COLUMNAS)
     size_header= LENGTH_CANT_COLUMNAS + (LENGTH_COLUMNA * cantColumnas)
     return size_header
@@ -25,7 +26,7 @@ def lengthHeader():
 def readHeader():
     titulos=[]
     caracteres=[]
-    with open('nombrearchivo.txt','r') as archivo:
+    with open(PATH_ARCHIVO,'r') as archivo:
         cantColumnas=int(archivo.read(LENGTH_CANT_COLUMNAS))
         for i in range(0,cantColumnas):
             titulos.append(archivo.read(LENGTH_NOMBRE_COLUMNA))
@@ -34,23 +35,25 @@ def readHeader():
 
 def lenghData():
     size_data=0
-    with open('nombrearchivo.txt','r') as archivo:
+    with open(PATH_ARCHIVO,'r') as archivo:
         cantColumnas=int(archivo.read(LENGTH_CANT_COLUMNAS))
         for i in range(0,cantColumnas):
             longitud=(int (archivo.read(LENGTH_CARACTERES)))
             size_data+= longitud
     return size_data    
 
+def getOffset(index):
+    return lengthHeader() + (lenghData * (index-1))
 
 
 def readByPK(index):
-    pos= lengthHeader() + (lenghData * (index-1))
+    pos= getOffset
     return readByOffset(pos)
 
 def readByOffset(pos):
     columnas=[]
     data=[]
-    with open('nombrearchivo.txt','r') as archivo:
+    with open(PATH_ARCHIVO,'r') as archivo:
         cantColumnas= int(archivo.read(LENGTH_CANT_COLUMNAS))
         archivo.seek(pos)
         for i in range(0,cantColumnas):
@@ -58,12 +61,31 @@ def readByOffset(pos):
             data[i].append(archivo.read(LENGTH_CARACTERES))
     return columnas,data
 
-
+def writeByPk(index,datos):
+    if index is not None:
+        pos = getOffset(index)
+    else:
+        pos = None
+    writeByOffset(pos,datos)
     
+
+def writeByOffset(pos,datos):
+    titulos,caracteres= readHeader()
+    with open(PATH_ARCHIVO,'r+b') as archivo:
+        if pos is None:
+            archivo.seek(0,2)
+        else:
+            archivo.seek(pos)
+        for i in range(0, len(datos)):
+            archivo.write(datos[i].ljust(int(caracteres[i])).encode("utf-8"))  
+    
+
+def insert(index,datos):
+    writeByPk(index,datos)
 
 def mostrarArchivo():
     print("---------Info Header--------------------\n")
-    with open('nombrearchivo.txt','r') as archivo:
+    with open(PATH_ARCHIVO,'r') as archivo:
         print(f"Cantidad de columnas: {archivo.read(LENGTH_CANT_COLUMNAS)} \n")
         print(f'Espacio que ocupa el nombre de columna: {LENGTH_NOMBRE_COLUMNA}\n')
         print(f'Espacio que ocupa el dato de la columna: {LENGTH_CARACTERES}\n')
