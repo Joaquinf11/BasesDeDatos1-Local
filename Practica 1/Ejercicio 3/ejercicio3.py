b09706d0fcaf731ad2c12eef82731bfbc39954f5
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-08-30 20:01:59 -0300

    Ej3 listo con correciones de clase

--- Practica 1/Ejercicio 3/ejercicio3.py
+++ Practica 1/Ejercicio 3/ejercicio3.py
@@ -2,48 +2,48 @@ LENGTH_CANT_COLUMNAS = 2
 LENGTH_NOMBRE_COLUMNA = 16
 LENGTH_CARACTERES = 4
 
-
 LENGTH_COLUMNA = LENGTH_NOMBRE_COLUMNA + LENGTH_CARACTERES  # tamaño de columna
+
+PADDING=" "
+ESTADO_LONGITUD=1
+ESTADO_ACTIVA="A"
+ESTADO_BAJA="B"
+TITULOS=0
+CARACTERES=1
 
 PATH_ARCHIVO= 'nombrearchivo.txt'
 
+class Metadata:
+    def __init__(self):
+        self.cantidad_columnas= 1
+        self.titulos= ["Estado".ljust(LENGTH_NOMBRE_COLUMNA)]
+        self.caracteres=[ESTADO_LONGITUD]
+        self.size_data= ESTADO_LONGITUD #HAY QUE INICIARLO CON ESTADO_LONGITUD
+        self.size_head=0
+
+METADATA = Metadata()
+
 def generarArchivo():
-    cantColumnas=input("Ingrese la cantidad de columnas: ")
+    cantidad_columnas=input("Ingrese la cantidad de columnas: ")
     with open(PATH_ARCHIVO,'w') as archivo:
-        archivo.write(cantColumnas.ljust(LENGTH_CANT_COLUMNAS))
-        for i in range(int(cantColumnas)):
-            archivo.write(input(f"inserte el titulo de la columna numero {i}: ").ljust(LENGTH_NOMBRE_COLUMNA))
-            archivo.write(input(f"inserte la cantidad de caracteres utilizados por dato en la columna numero {i}: ").ljust(LENGTH_CARACTERES))
+        archivo.write(cantidad_columnas.ljust(LENGTH_CANT_COLUMNAS,PADDING))
+        for i in range(int(cantidad_columnas)):
+            archivo.write(input(f"inserte el titulo de la columna numero {i}: ").ljust(LENGTH_NOMBRE_COLUMNA,PADDING))
+            archivo.write(input(f"inserte la cantidad de caracteres utilizados por dato en la columna numero {i}: ").ljust(LENGTH_CARACTERES,PADDING))
 
-def lengthHeader():
+def readHead(): 
     with open(PATH_ARCHIVO,'r') as archivo:
-        cantColumnas=archivo.read(LENGTH_CANT_COLUMNAS)
-    size_header= LENGTH_CANT_COLUMNAS + (LENGTH_COLUMNA * (int(cantColumnas)))
-    return size_header
-
-
-def readHeader():
-    titulos=[]
-    caracteres=[]
-    with open(PATH_ARCHIVO,'r') as archivo:
-        cantColumnas=int(archivo.read(LENGTH_CANT_COLUMNAS))
-        for i in range(0,cantColumnas):
-            titulos.append(archivo.read(LENGTH_NOMBRE_COLUMNA))
-            caracteres.append(int(archivo.read(LENGTH_CARACTERES)))
-    return titulos,caracteres
-
-def lenghData():
-    size_data=0
-    titulos,caracteres= readHeader()
-    for i in range(0,len(caracteres)):
-            longitud=(int (caracteres[i]))
-            size_data+= longitud
-    return size_data    
+        cantidad_columnas=int(archivo.read(LENGTH_CANT_COLUMNAS))
+        METADATA.cantidad_columnas=cantidad_columnas + 1 # mas uno por agregar ESTADO
+        METADATA.size_head= LENGTH_CANT_COLUMNAS + (LENGTH_COLUMNA * (int(cantidad_columnas)))
+        for i in range(0,cantidad_columnas):
+            METADATA.titulos.append(archivo.read(LENGTH_NOMBRE_COLUMNA))
+            caracteres=int(archivo.read(LENGTH_CARACTERES))
+            METADATA.caracteres.append(caracteres)
+            METADATA.size_data +=  caracteres
 
 def getOffset(index):
-    size_header=lengthHeader()
-    size_data= lenghData()
-    pos= size_header + (size_data * ((int(index))-1))
+    pos= METADATA.size_head + (METADATA.size_data * ((int(index))-1))
     return pos
 
 
@@ -52,12 +52,11 @@ def readByPK(index):
     return readByOffset(pos)
 
 def readByOffset(pos):
-    titulos,caracteres=readHeader()
     datos=[]
     with open(PATH_ARCHIVO,'r') as archivo:
         archivo.seek(pos)
-        for i in range(0,len(caracteres)):
-            datos.append(archivo.read(caracteres[i]))
+        for i in range(0,METADATA.cantidad_columnas):
+            datos.append(archivo.read(METADATA.caracteres[i]))
     return datos
 
 def writeByPK(index,datos):
@@ -69,14 +68,14 @@ def writeByPK(index,datos):
     
 
 def writeByOffset(pos,datos):
-    titulos,caracteres= readHeader()
     with open(PATH_ARCHIVO,'r+b') as archivo:
         if pos is None:
             archivo.seek(0,2)
         else:
             archivo.seek(pos)
-        for i in range(0, len(datos)):
-            archivo.write(datos[i].ljust(int(caracteres[i])).encode("utf-8"))  
+        archivo.write(ESTADO_ACTIVA.encode("utf-8"))
+        for i in range(0, METADATA.cantidad_columnas-1):
+            archivo.write(datos[i].ljust(METADATA.caracteres[i+1],PADDING).encode("utf-8"))
     
 
 def insert(index,datos):
@@ -84,48 +83,43 @@ def insert(index,datos):
 
 def delete(index):
     pos=getOffset(index)
-    size_data=lenghData()
-    with open(PATH_ARCHIVO,'r+b') as archivo:
-        archivo.seek(-size_data,2)
-        ultimo_registro= archivo.read(size_data)
-        archivo.seek(pos)
-        archivo.write(ultimo_registro)
-        archivo.seek(-size_data,2)
-        archivo.truncate()
+    with open(PATH_ARCHIVO,'r+') as archivo:
+       archivo.seek(pos)
+       archivo.write(ESTADO_BAJA)
+
 
 def update(index,new_datos):
-    #old_datos = readByPK(index). FALTARIA preguntar si alguno de los datos nuevos es none para reemplazarlo por el old correspondiente
+    old_datos = readByPK(index)
+    if old_datos[0] == "B":
+        print("No se encuentra cargado el registro")
+        return
+    for i in range(0,METADATA.cantidad_columnas-1):
+        if new_datos[i].isspace():
+            new_datos[i]= old_datos[i+1]
     writeByPK(index,new_datos)
 
 def mostrarArchivo():
-    # print("---------------------------Info Header---------------------------\n")
-    # with open(PATH_ARCHIVO,'r') as archivo:
-    #     print(f"Cantidad de columnas: {archivo.read(LENGTH_CANT_COLUMNAS)} \n")
-    #     print(f'Espacio que ocupa el nombre de columna: {LENGTH_NOMBRE_COLUMNA}\n')
-    #     print(f'Espacio que ocupa el dato de la columna: {LENGTH_CARACTERES}\n')
-    #     print("-----------------------------------------------------------------\n")
-        
-        titulos,caracteres=readHeader()
-        print("----------------------------------HEADER------------------------------\n")
-        for i in range(0,len(caracteres)):
-            print(f"Columna {i}: {titulos[i].strip()}  || Longitud: {caracteres[i]}") 
-        print("----------------------------------------------------------------------\n")
+    print("----------------------------------Head------------------------------\n")
+    for i in range(1,METADATA.cantidad_columnas):
+        print(f"Columna {i}: {METADATA.titulos[i]}  || Longitud: {METADATA.caracteres[i]}") 
+    print("----------------------------------------------------------------------\n")
 
-        
-        index=1
-        while True:
-            datos=readByPK(index)
-            if datos[0] == "":
-                break
+    index=1
+    while True:
+        datos=readByPK(index)
+        if datos[0] == "":
+            break
+        elif datos[0] == "B":
+            index +=1
+        else:
             print("-----------------------------")
-            for i in range(0,len(datos)):
-                    print(f"{titulos[i].strip()}: {datos[i]}")
+            for i in range(1,METADATA.cantidad_columnas):
+                    print(f"{METADATA.titulos[i].strip()}: {datos[i]}")
             index+=1
-        print("-----------------------------\n")
+            print("-----------------------------\n")
 
 def ingresarDatos():
-    titulos,caracteres= readHeader()
     datos = []
-    for i in range(len(caracteres)):
-        datos.append(input(f"{titulos[i].strip()}: ").ljust(caracteres[i]))
+    for i in range(1,METADATA.cantidad_columnas):
+        datos.append(input(f"{METADATA.titulos[i].strip()}: ").ljust(METADATA.caracteres[i],PADDING))
     return datos
--- Practica 1/Ejercicio 3/main_ej3.py
+++ Practica 1/Ejercicio 3/main_ej3.py
@@ -3,53 +3,58 @@ import ejercicio3
 def mostrarMenu():
     print("\n ABM de Archivo de Metadata \n")
     print("0- Salir\n" +
-          "1 - Generar archivo nuevo \n" +
           "2 - Mostrar archivo\n" +
           "3 - Alta\n" + 
           "4 - Modificacion\n" +
-          "5 - Baja\n"
-          )
+          "5 - Baja\n")
 
 def main():
+    print("0- Salir\n" +
+        "1 - Generar archivo nuevo \n" +
+        "2 - Manipular archivo existente\n")
+    
+    accion=input("Elija una opcion:"  )
+    if accion == '0':
+        return
+    if accion == '1':
+       
+            ejercicio3.generarArchivo()
+            print("Archivo generado exitosamente.")
+       
+            return
+
     mostrarMenu()
     accion= input("Ingrese una opcion: ")
 
+    ejercicio3.readHead()
+
     while(accion != '0'):
-        if accion == '1':
-            try:
-                ejercicio3.generarArchivo()
-                print("Archivo generado exitosamente.")
-            except Exception as e:
-                print(f"Error al generar archivo: {e}")
-        elif accion == '2':
-           # try:
+        if accion == '2':
+           
                 ejercicio3.mostrarArchivo()
-            # except Exception as e:
-            #     print(f"Error al mostrar archivo: {e}")
+            
         elif accion == '3':
-            try:
+           
                 print("Ingrese los datos")
                 datos= ejercicio3.ingresarDatos()
                 ejercicio3.insert(None, datos)
                 print("Alta generada exitosamente.")
-            except Exception as e:
-                print(f"Error al dar de alta: {e}")
+            
+                
         elif accion == '4':
-            try:
+           
                 index=input("Ingrese el indice que desea modificar: ")
                 print("Ingrese los datos")
                 datos= ejercicio3.ingresarDatos()
                 ejercicio3.update(index, datos)
                 print("Modificacion generada exitosamente.")
-            except Exception as e:
-                print(f"Error al realizar modificacion: {e}")
+           
+                
         elif accion == '5':
-            try:
                 index=input("Ingrese el indice que desea eliminar: ")
                 ejercicio3.delete(index)
                 print("Registro eliminado exitosamente.")
-            except Exception as e:
-                print(f"Error al dar de baja: {e}")
+        
         
         mostrarMenu()
         accion=input("Ingrese una opcion:")
